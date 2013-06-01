aa3cd88bbb38ed108e61a727db6d4eb3eb070e94
Michael Bragg <mike.a.bragg@gmail.com>
2013-06-01 15:02:34 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialinn::Application.config.secret_token = '09294972d6f418d9ae0cdc1929364430347965e70b0fcab1b5e36505d3056e3359f538f7c03f2c5d0dfbb34706ba3396e5c486be25986fd5f09f5f9f026612f5'
